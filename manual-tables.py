651a54e916923971b6b7dfae912babc90fd8e6d6
Matthew Malcomson <asoneth@Matthews-MacBook-Air.local>
2024-06-17 18:37:59 +0530

    Formatting of the action table
    No functional change.

--- manual-tables.py
+++ manual-tables.py
@@ -32,78 +32,85 @@ def accept():
     return _accept_
 
 action_table = [
-    # s0
-    {'Add':         shift(1),
-     'Factor':      shift(2),
-     'Term':        shift(3),
-     '(':           shift(4),
-     'name':        shift(5),
-     'int':         shift(6)},
-    # s1
-    {'$':           accept(),
-     '+':           shift(7)},
-    # s2
-    {'$':           red(1, 'Add'),
-     '+':     red(1, 'Add'),
-     ')':   red(1, 'Add'),
-     '*':   shift(8)},
-    # s3
-    {'$':           red(1, 'Factor'),
-     '*':     red(1, 'Factor'),
-     '+':   red(1, 'Factor'),
-     ')':   red(1, 'Factor')},
-    # s4
-    {'Add':           shift(9),
-     'Factor':     shift(2),
-     'Term':     shift(3),
-     '(':     shift(4),
-     'name':     shift(5),
-     'int':   shift(6)},
-    # s5
-    {'$':           red(1, 'Term'),
-     '+':     red(1, 'Term'),
-     '*':   red(1, 'Term'),
-     ')':   red(1, 'Term')},
-    # s6
-    {'$':           red(1, 'Term'),
-     '+':     red(1, 'Term'),
-     '*':   red(1, 'Term'),
-     ')':   red(1, 'Term')},
-    # s7
-    {'Factor':           shift(11),
-     'Term':     shift(3),
-     '(':     shift(4),
-     'name':     shift(5),
-     'int':     shift(6),
-    },
-    # s8
-    {'Term':     shift(10),
-     '(':     shift(4),
-     'name':     shift(5),
-     'int':     shift(6),
-    },
-    # s9
-    {')':           shift(12),
-     '+':     shift(7),
-    },
-    # s10
-    {'$':           red(3, 'Factor'),
-     '*':           red(3, 'Factor'),
-     '+':           red(3, 'Factor'),
-     ')':           red(3, 'Factor'),
-    },
-    # s11
-    {'$':           red(3, 'Add'),
-     ')':           red(3, 'Add'),
-     '+':           red(3, 'Add'),
-     '*':           shift(8),
-    },
-    # s12
-    {'$':           red(3, 'Term'),
-     '*':           red(3, 'Term'),
-     '+':           red(3, 'Term'),
-     ')':           red(3, 'Term'),
-    }]
+        # s0
+        {'Add':     shift(1),
+        'Factor':   shift(2),
+        'Term':     shift(3),
+        '(':        shift(4),
+        'name':     shift(5),
+        'int':      shift(6)
+        },
+        # s1
+        {'$':       accept(),
+        '+':        shift(7)
+        },
+        # s2
+        {'$':       red(1,      'Add'),
+        '+':        red(1,      'Add'),
+        ')':        red(1,      'Add'),
+        '*':        shift(8)
+        },
+        # s3
+        {'$':       red(1,      'Factor'),
+        '*':        red(1,      'Factor'),
+        '+':        red(1,      'Factor'),
+        ')':        red(1,      'Factor')
+        },
+        # s4
+        {'Add':     shift(9),
+        'Factor':   shift(2),
+        'Term':     shift(3),
+        '(':        shift(4),
+        'name':     shift(5),
+        'int':      shift(6)
+        },
+        # s5
+        {'$':       red(1,      'Term'),
+        '+':        red(1,      'Term'),
+        '*':        red(1,      'Term'),
+        ')':        red(1,      'Term')
+        },
+        # s6
+        {'$':       red(1,      'Term'),
+        '+':        red(1,      'Term'),
+        '*':        red(1,      'Term'),
+        ')':        red(1,      'Term')
+        },
+        # s7
+        {'Factor':  shift(11),
+        'Term':     shift(3),
+        '(':        shift(4),
+        'name':     shift(5),
+        'int':      shift(6),
+        },
+        # s8
+        {'Term':    shift(10),
+        '(':        shift(4),
+        'name':     shift(5),
+        'int':      shift(6),
+        },
+        # s9
+        {')':       shift(12),
+        '+':        shift(7),
+        },
+        # s10
+        {'$':       red(3,      'Factor'),
+        '*':        red(3,      'Factor'),
+        '+':        red(3,      'Factor'),
+        ')':        red(3,      'Factor'),
+        },
+        # s11
+        {'$':       red(3,      'Add'),
+        ')':        red(3,      'Add'),
+        '+':        red(3,      'Add'),
+        '*':        shift(8),
+        },
+        # s12
+        {'$':       red(3,      'Term'),
+        '*':        red(3,      'Term'),
+        '+':        red(3,      'Term'),
+        ')':        red(3,      'Term'),
+        }]
 
 if __name__ == '__main__':
     print('\nNext\n\n')
