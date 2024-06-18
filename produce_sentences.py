c25f2e4861345bfa70bac568b78519d6097ee336
Matthew Malcomson <asoneth@Matthews-MacBook-Air.local>
2024-06-19 00:01:01 +0530

    Add some more robust testing
    Few different things in order to make this work:
    - Grammar file slightly different format (having some terminals in
      double-quotes and some not was strange and didn't fit what I'd done
      with the manual tables).
    - Introduce file to produce valid random sentences from my grammar
      (reading in the grammar from a file).
    - Add tests that parsing text generated from a grammar gives the same
      answers as parsing the tokens generated from the grammar.
      I.e. that my generator matches my parser & tokenizer, plus that there
      is nothing in the tokenizer step that changes the answer from the
      parser.

---
+++ produce_sentences.py
@@ -0,0 +1,96 @@
+import collections
+import copy
+import itertools as itt
+import random
+import string
+import logging
+logger = logging.getLogger(__name__)
+
+def split_strip(line):
+    return [x.strip() for x in line.split()]
+
+def single_rule(line):
+    k, r = line.split('=')
+    return (k.strip(), split_strip(r))
+
+def get_rules(text):
+    ret = collections.defaultdict(list)
+    for line in text.splitlines():
+        if line.startswith('//'):
+            continue
+        line = line.strip()
+        if not line:
+            continue
+        k, r = single_rule(line)
+        assert(r)
+        ret[k].append(r)
+    return ret
+
+def terminal(sym, rules):
+    return sym not in rules
+
+def make_word(terminal):
+    if terminal == 'name':
+        return random.choice(string.ascii_lowercase[:7])
+    if terminal == 'int':
+        return str(random.randint(0, 40))
+    return terminal
+
+class NonTerm:
+    '''Closure to call 'one_level_produce', but with repr for logging.'''
+    def __init__(self, rules, nonterm):
+        self.rules = rules
+        self.nonterm = nonterm
+    def __call__(self, depth):
+        return one_level_produce(self.rules, self.nonterm, depth)
+    def __repr__(self):
+        return self.nonterm
+    def __str__(self):
+        return self.nonterm
+
+def choose_min(productions, rules):
+    def find_cost(p):
+        return sum(int(terminal(sym, rules)) for sym in p)
+    return min(productions, key=find_cost)
+
+def one_level_produce(rules, nonterm, depth):
+    logger.debug('one_level_produce -- {}'.format(nonterm))
+    productions = rules[nonterm]
+    if depth > 10:
+        cur_choice = choose_min(productions, rules)
+    else:
+        cur_choice = random.choice(productions)
+    logger.debug('choosing {}'.format(cur_choice))
+    return [(x, make_word(x)) if terminal(x, rules) else
+            NonTerm(rules, x) for x in cur_choice]
+
+def produce(rules, nonterm, depth=0):
+    '''A little convoluted in order to avoid recusion problems.
+    Not only have a loop in order to avoid recursion, but also have to avoid
+    the fact that when we have multiple nonterminals we are more likely to
+    expand something into nonterminals.
+    '''
+    cur_level = one_level_produce(rules, nonterm, depth)
+    depth += 1
+    while True:
+        adjusted = False
+        logger.debug(' '.join(str(x) for x in cur_level))
+        next_level = []
+        for sym in cur_level:
+            if callable(sym):
+                next_level.extend(sym(depth))
+                depth += 1
+                adjusted = True
+            else:
+                next_level.append(sym)
+        cur_level = next_level
+        if not adjusted:
+            break
+    return cur_level
+
+if __name__ == '__main__':
+    import default_log_arg
+    default_log_arg.do_default_logarg()
+    with open('tutorial-grammar.txt') as infile:
+        text = infile.read()
+    all_rules = get_rules(text)
--- tests.py
+++ tests.py
@@ -1,14 +1,71 @@
 import unittest
+import operator
+import random
 from parsing_from_text import parse_from_string
+import itertools as itt
+import manual_tables
+import produce_sentences
+
+def merge_sentence_as_string(sent):
+    # N.b. All tokens can be directly after each other (i.e. without
+    # whitespace) as far as the tokenizer is concerned -- *except* names being
+    # directly after integers.
+    # However, since we generate *valid* grammar, and a name is not allowed
+    # directly after a digit, we don't need to worry about that for this
+    # function.
+    #   - For testing posibly bad tokens we may need to worry about this.
+    #
+    # N.b. including '' twice in order to increase possibility of no whitespace
+    # in between tokens.
+    def random_choices(spaces):
+        while True:
+            yield random.choice(spaces)
+    def flat_zip(x, y):
+        for a, b in zip(x, y):
+            yield a
+            yield b
+    all_tokens = map(operator.itemgetter(1), sent)
+    spaces = (a+b for a,b in itt.product(['', '\n', '\t', ' ', ''], repeat=2))
+    random_spaces = random_choices(list(spaces))
+    return ''.join(flat_zip(all_tokens, random_spaces))
 
 class TestManualExpressions(unittest.TestCase):
-    def test_basic_addition(self):
+    test_rules = '''
+    Start  = Add
+
+    Add    = Add + Factor
+    Add    = Factor
+
+    Factor = Factor * Term
+    Factor = Term
+
+    Term   = ( Add )
+    Term   = name
+    Term   = int
+    '''
+    def test_basic(self):
         basic_exp = parse_from_string('x+y')
         known_ret = [':Add', [':Add', [':Factor', [':Term', 'x']]], '+', [':Factor', [':Term', 'y']]]
         self.assertEqual(basic_exp, known_ret)
         self.assertEqual(parse_from_string('x + y'), known_ret)
         self.assertEqual(parse_from_string('x+ y '), known_ret)
         self.assertEqual(parse_from_string('x+ y\n'), known_ret)
+        known_ret = [':Add', [':Factor', [':Term', 'x']]]
+        self.assertEqual(parse_from_string('x'), known_ret)
+    def test_parser_accepts(self):
+        rules = produce_sentences.get_rules(self.test_rules)
+        all_keys = list(rules.keys())
+        for _ in range(1000):
+            gen_key = random.choice(all_keys)
+            generated = produce_sentences.produce(rules, gen_key)
+            st = manual_tables.State()
+            for ty, text in generated:
+                manual_tables.advance(st, ty, text)
+            manual_tables.advance(st, '$', '')
+            self.assertTrue(st.accepted_expressions)
+            directly = st.accepted_expressions[0]
+            via_text = parse_from_string(merge_sentence_as_string(generated))
+            self.assertEqual(via_text, directly)
 
 if __name__ == '__main__':
     unittest.main()
