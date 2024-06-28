a46c41d9769d719e0be46517b54a0dfacd13fdab
Matthew Malcomson <asoneth@Matthews-MacBook-Air.local>
2024-06-28 15:24:37 +0530

    Looks like implemented LR(1) parsing
    Have not tested this thoroughly, so highly likely to be some problems left to fix, but this at least appears to be doing what it should.
    As it turns out the changes were mostly mechanical for everything except finding a FOLLOW set for each itemset.
    Determining the follow set for a particular itemset is quite different to determining the follow set as a whole.
    Some predictions in the itemset do not get updated etc.

--- canonical_lr_generator.py
+++ canonical_lr_generator.py
@@ -1,4 +1,13 @@
-# Starting with the SLR code that I have before
+# Starting with the SLR code that I have before.
+# Idea is that the grammar I have in 'slr_lr_grammar.txt' should not work with
+# this (as demonstrated by a local test), but should work after modifications.
+#
+# Changes required for LR(1) generator:
+#   - When generating new item set, also generate FOLLOW sets for a given
+#     production.
+#   - Hence need to store the FOLLOW set for a given production on the
+#     production.
+#   - Also, the FOLLOW set is generated in a completely different way.
 
 
 from parse_grammar import get_rules
@@ -50,8 +59,13 @@ def nullable_syms(rules):
 #######  FIRST set.
 def first(rules, nullable):
     # At one point I was concerned that FIRST(x) could need FOLLOW to figure out.
-    # I.e. when x is nullable.  I think this is not a problem since FIRST is only used in FOLLOW, so if I make sure to check for x being nullable when determining FOLLOW I shouldn't need to take it into account when defining FIRST.
-    # Instead, FIRST just needs to find everything that *could* start a given nonterminal.  The possibility that a given nonterminal may expand to nothing doesn't need to be represented in this data structure.
+    # I.e. when x is nullable.  I think this is not a problem since FIRST is
+    # only used in FOLLOW, so if I make sure to check for x being nullable when
+    # determining FOLLOW I shouldn't need to take it into account when defining
+    # FIRST.
+    # Instead, FIRST just needs to find everything that *could* start a given
+    # nonterminal.  The possibility that a given nonterminal may expand to
+    # nothing doesn't need to be represented in this data structure.
     SymStore = collections.namedtuple('SymStore', ['terminals', 'nonterminals'])
     first_items = collections.defaultdict(lambda: SymStore(set(), set()))
     for key, val in rules.items():
@@ -75,46 +89,6 @@ def first(rules, nullable):
         return ret
     finalised_first = transitive_closure_mutable(first_items, update_first)
     return {key: val.terminals for key, val in finalised_first.items()}
-
-####### FOLLOW set
-def follow(rules, first, nullable, known):
-    SymStore = collections.namedtuple('SymStore', ['terminals', 'follow'])
-    follow_items = {key: SymStore(set(), set()) for key in rules}
-    for k, v in known.items():
-        follow_items[k].terminals.update(v)
-    for key, val in rules.items():
-        for gen in val:
-            update_list = []
-            for sym in gen:
-                for s in update_list:
-                    item = follow_items[s]
-                    if terminal(sym):
-                        item.terminals.add(sym)
-                    else:
-                        item.terminals.update(first[sym])
-                if terminal(sym):
-                    update_list = []
-                elif sym not in nullable:
-                    update_list = [sym]
-                else:
-                    update_list.append(sym)
-            # Above loop handles all up to last element, this handles
-            # everything "active" at end of rule.
-            for s in update_list:
-                item = follow_items[s]
-                item.follow.add(key)
-    def update_follow(current_follow):
-        logger.debug('update_follow: ' + str(current_follow))
-        ret = False
-        for key, this in current_follow.items():
-            for chain in this.follow:
-                that = current_follow[chain]
-                if not this.terminals.issuperset(that.terminals):
-                    ret = True
-                    this.terminals.update(that.terminals)
-        return ret
-    finalised_follow = transitive_closure_mutable(follow_items, update_follow)
-    return {k: v.terminals for k, v in finalised_follow.items()}
     
 ####### Itemsets
 # Structure of an SLR itemset is:
@@ -126,15 +100,17 @@ class Prediction:
     key: str
     gen: tuple[str, ...]
     idx: int
+    follow_set: frozenset[str, ...]
     def next_sym(self):
         return self.gen[self.idx] if self.idx < len(self.gen) else None
     def shifted(self):
         assert(self.idx < len(self.gen))
-        return Prediction(self.key, self.gen, self.idx+1)
+        return Prediction(self.key, self.gen, self.idx+1, self.follow_set)
     def __str__(self):
-        return '{} -> {} . {}'.format(self.key,
+        return '{} -> {} . {}\t\t{}'.format(self.key,
                                       ' '.join(self.gen[:self.idx]),
-                                      ' '.join(self.gen[self.idx:]))
+                                      ' '.join(self.gen[self.idx:]),
+                                      str(self.follow_set))
 @dataclass(frozen=True)
 class ItemSet:
     predictions: frozenset[Prediction, ...]
@@ -181,6 +157,9 @@ def extend_predictions(rules, predictions):
     '''Expand a kernel of an itemset into a full itemset.
     NOTE: This can lead to duplicates of the items in the kernel, but we assume
     that's handled by ItemSet making things unique.'''
+    # TODO think about whether I need to add nullable symbols here too.
+    # I.e. with a rule of X -> a . B C d, where B can be null, do I add C as well?
+    # I think not ... would like to confirm.
     logger.debug('extending: ' + str(predictions))
     tohandle = [p.next_sym() for p in predictions]
     seen = set()
@@ -191,19 +170,80 @@ def extend_predictions(rules, predictions):
         if sym in seen or sym is None:
             continue
         seen.add(sym)
-        extra = [Prediction(sym, tuple(x), 0) for x in rules[sym]]
+        extra = [Prediction(sym, tuple(x), 0, frozenset()) for x in rules[sym]]
         predictions.extend(extra)
         tohandle.extend(x.next_sym() for x in extra)
     return predictions
 
-def actions_for(predictions, root_term, follow):
+def update_follows(predictions, first, nullable):
+    SymStore = collections.namedtuple('SymStore', ['terminals', 'follow'])
+    # The below set of predictions is intereting to know about, but not
+    # important to implement the algorithm.
+    # already_known = [p for p in predictions if p.idx != 0]
+    #
+    # We use all predictions for updating the FOLLOW set, then we add that
+    # follow set to those predictions that are just starting.
+    follow_items = {p.key: SymStore(set(p.follow_set), set())
+                    for p in predictions if p.idx == 0}
+    for p in predictions:
+        ns = p.next_sym()
+        if terminal(ns):
+            continue
+        update_list = [ns]
+        for sym in p.gen[p.idx+1:]:
+            for s in update_list:
+                item = follow_items[s]
+                if terminal(sym):
+                    item.terminals.add(sym)
+                else:
+                    item.terminals.update(first[sym])
+            if sym not in nullable:
+                break
+            else: # N.b. must be a non-terminal because it's in `nullable`.
+                update_list.append(sym)
+        else:
+            # Above loop handles all up to last element.
+            # If in this clause then the remainder of this rule is nullable
+            # as a whole.
+            for s in update_list:
+                item = follow_items[s]
+                if p.idx != 0:
+                    # Already know the follow set that this has.  Follow set
+                    # not affected by calculation of follow set in this group.
+                    item.terminals.update(p.follow_set)
+                else:
+                    logger.info('sym: {} in prediction: {}'.format(s, p))
+                    item.follow.add(p.key)
+    def update_follow(current_follow):
+        logger.debug('update_follow: ' + str(current_follow))
+        ret = False
+        for key, this in current_follow.items():
+            for chain in this.follow:
+                that = current_follow[chain]
+                if not this.terminals.issuperset(that.terminals):
+                    ret = True
+                    this.terminals.update(that.terminals)
+        return ret
+    finalised_follow = transitive_closure_mutable(follow_items, update_follow)
+    result = []
+    for p in predictions:
+        if p.idx != 0:
+            result.append(p)
+            continue
+        result.append(Prediction(p.key, p.gen, p.idx,
+                     frozenset(finalised_follow[p.key].terminals)))
+    logger.debug('update_follows:  {}'.format(pprint.pformat(result)))
+    return result
+
+
+def actions_for(predictions, root_term):
     ret = collections.defaultdict(list)
     reductions = {}
     accepts = set()
     for p in predictions:
         sym = p.next_sym()
         if sym is None:
-            for f in follow[p.key]:
+            for f in p.follow_set:
                 # Early assertion error on shift/reduce conflict.
                 assert(f not in ret)
                 # Assertion error on reduce/reduce conflict.
@@ -222,15 +262,20 @@ def actions_for(predictions, root_term, follow):
 #   - Hash on kernels rather than entire states?
 #     Should do essentially the same thing, but could save in "seen"
 #     before expanding (saving a bit of work).
-def itemlists(rules, root_term, follow):
+def itemlists(rules, root_term, root_follow, first, nullable):
     # Approach:
     #  1) Expand all implicit states from the beginning of root_term.
     #     - This gives the first itemlist.
     #  2) For each symbol in any "next" position:
     #     - Create the kernel of a new itemset.
     #     - Expand all implicit states.
-    predictions = [Prediction(root_term, tuple(x), 0) for x in rules[root_term]]
+    def add_follows(predictions):
+        return update_follows(predictions, first, nullable)
+    predictions = [Prediction(root_term, tuple(x), 0,
+                              frozenset(root_follow))
+                    for x in rules[root_term]]
     extend_predictions(rules, predictions)
+    predictions = add_follows(predictions)
     start = ItemSet.from_iterable(predictions)
     counter = 0
     seen = {}
@@ -244,10 +289,10 @@ def itemlists(rules, root_term, follow):
             continue
         seen[s] = counter
         counter += 1
-        reductions, shifts, accepts = actions_for(s.predictions, root_term, follow)
+        reductions, shifts, accepts = actions_for(s.predictions, root_term)
         for sym in shifts:
             extend_predictions(rules, shifts[sym])
-            toadd = ItemSet.from_iterable(shifts[sym])
+            toadd = ItemSet.from_iterable(add_follows(shifts[sym]))
             shifts[sym] = toadd
             if toadd not in seen:
                 tohandle.append(toadd)
@@ -291,14 +336,13 @@ def generate_action_tables(grammar_filename):
     terminal = make_terminal_func(nonterminals)
     FIRST = first(all_rules, nullable)
     logger.info('FIRST: ' + str(FIRST))
-    FOLLOW = follow(all_rules, FIRST, nullable, {'Start': ['$']})
-    logger.info('FOLLOW: ' + str(FOLLOW))
-    states = itemlists(all_rules, 'Start', FOLLOW)
+    states = itemlists(all_rules, 'Start', ['$'], FIRST, nullable)
     logger.info('States: ' + str(states))
     return convert_to_action_table(states, 'Start')
 
 def initialise_actions(grammar_filename):
     action_table = generate_action_tables(grammar_filename)
+    logger.info('action_tables: ' + pprint.pformat(action_table))
     manual_tables.initialise_actions(action_table)
 
 if __name__ == '__main__':
@@ -306,9 +350,11 @@ if __name__ == '__main__':
     import sys
     import parsing_from_text
     import default_log_arg
-    default_log_arg.do_default_logarg()
-    initialise_actions('slr_lr_grammar.txt')
-    logger.info('action_tables: ' + pprint.pformat(action_table))
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--grammar', default='slr_lr_grammar.txt')
+    args = default_log_arg.add_default_logarg(parser)
+    initialise_actions(args.grammar)
     parsed_expression = parsing_from_text.parse_from_string(sys.stdin.read())
     pprint.pprint(parsed_expression)
     
--- default_log_arg.py
+++ default_log_arg.py
@@ -4,9 +4,13 @@ import argparse
 
 def do_default_logarg():
     parser = argparse.ArgumentParser()
+    add_default_logarg(parser)
+
+def add_default_logarg(parser):
     parser.add_argument('--loglevel', default='error',
                         choices=['notset', 'debug', 'info', 'warning', 'error',
                                 'critical'])
     args = parser.parse_args()
     logging.basicConfig(stream=sys.stdout,
                         level=logging.__dict__[args.loglevel.upper()])
+    return args
